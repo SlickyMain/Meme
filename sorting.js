8ab7f5aadca43cff5983257dd073c7dad0917e27
Eugene <Slickylock@gmail.com>
2022-02-16 16:29:21 +0500

    + fetch

--- sorting.js
+++ sorting.js
@@ -1,95 +1,13 @@
-var listOfPosts = [
-    {
-        author: "Иванов",
-        dateOfPost: "12.01.2022 01:01",
-        description: "helohalowww",
-        likes: 68
-    },
-    {
-        author: "Петров",
-        dateOfPost: "05.16.2021 21:15",
-        description: "helohalowww",
-        likes: 26
-    },
-    {
-        author: "Астафьев",
-        dateOfPost: "03.03.2022 01:01",
-        description: "helohalowww",
-        likes: 106
-    },
-    {
-        author: "Сидоров",
-        dateOfPost: "10.01.2022 01:01",
-        description: "helohalowww",
-        likes: 13
-    },
-    {
-        author: "Чернов",
-        dateOfPost: "05.16.2021 21:15",
-        description: "helohalowww",
-        likes: 8
-    },
-    {
-        author: "Никонов",
-        dateOfPost: "03.09.2022 01:01",
-        description: "helohalowww",
-        likes: 1606
-    },
-    {
-        author: "Челикславич",
-        dateOfPost: "12.01.2022 01:01",
-        description: "helohalowww",
-        likes: 238
-    },
-    {
-        author: "Русских",
-        dateOfPost: "05.16.2021 21:15",
-        description: "helohalowww",
-        likes: 296
-    },
-    {
-        author: "Английских",
-        dateOfPost: "03.03.2022 01:01",
-        description: "helohalowww",
-        likes: 69
-    },
-    {
-        author: "Немецких",
-        dateOfPost: "12.01.2022 01:01",
-        description: "helohalowww",
-        likes: 23
-    },
-    {
-        author: "Усталов",
-        dateOfPost: "05.16.2021 21:15",
-        description: "helohalowww",
-        likes: 254
-    },
-    {
-        author: "Ййййъуьов",
-        dateOfPost: "03.03.2022 01:01",
-        description: "helohalowww",
-        likes: 502
-    },
-    {
-        author: "Куркума",
-        dateOfPost: "12.01.2022 01:01",
-        description: "helohalowww",
-        likes: 333
-    },
-    {
-        author: "Гречнёв",
-        dateOfPost: "05.16.2021 21:15",
-        description: "helohalowww",
-        likes: 2603
-    },
-    {
-        author: "Юсупов",
-        dateOfPost: "03.03.2022 01:01",
-        description: "helohalowww",
-        likes: 1852
-    }]
-
+var listOfPosts = fetch("http://meme.gcqadev.ru/post.json")
+    .then(res => {
+        return res.json()
+    })
+    .then(value => {
+        listOfPosts = value
+        for (kee of listOfPosts) {
+            kee.dateOfPost = new Date (kee.dateOfPost)
+        }
+    })
 
 function sortByAuthor() {
     let sortedByAuthor = listOfPosts.sort((a, b) => a.author > b.author ? 1 : -1)
@@ -98,9 +16,6 @@ function sortByAuthor() {
 }
 
 function sortByDate() {
-    for (value of listOfPosts) {
-        value.dateOfPost = new Date (value.dateOfPost)
-    }
     let sortedByAuthor = listOfPosts.sort((a, b) => a.dateOfPost > b.dateOfPost ? 1 : -1)
     insertInto(sortedByAuthor)
 }
@@ -117,7 +32,7 @@ function insertInto(completedArray) {
     for (let i = 0; i < completedArray.length; i++) {
         let post = template.content.cloneNode(true)
         post.querySelector("h3").innerText = completedArray[i].author
-        post.querySelector(".fieldForLike").innerText = completedArray[i].likes
+        post.querySelector(".fieldForLike").innerText = completedArray[i].likes + " likes"
         post.querySelector(".fieldForDate").innerText = completedArray[i].dateOfPost
         parentNode.append(post)
     }
