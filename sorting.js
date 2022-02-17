7a867eb2d52badcdc8baf50045db101b1dc2774e
Eugene <Slickylock@gmail.com>
2022-02-17 11:45:27 +0500

    delete items + infinity scroll

--- sorting.js
+++ sorting.js
@@ -1,34 +1,64 @@
-var listOfPosts = fetch("http://meme.gcqadev.ru/post.json")
+var porcionOfPosts
+var listOfPosts
+var loadedAlready = 0
+const parentNode = document.getElementById("postField")
+const placeholdForCounter = "Количество элементов на странице: "
+const counter = document.getElementById("counter")
+
+function requestToServer() {
+    fetch("http://meme.gcqadev.ru/post.json")
     .then(res => {
         return res.json()
     })
     .then(value => {
-        listOfPosts = value
-        for (kee of listOfPosts) {
+        if (!listOfPosts) {
+            listOfPosts = value
+            for (kee of value) {
+                kee.dateOfPost = new Date (kee.dateOfPost)
+            }
+        }
+        porcionOfPosts = value.length
+        loadedAlready += value.length
+        for (kee of value) {
             kee.dateOfPost = new Date (kee.dateOfPost)
         }
+        insertInto(value)
+        countElems(placeholdForCounter)
     })
+}
+
+function countElems (placehold) {
+    let displaysNow = parentNode.children.length
+    counter.innerHTML = placehold + displaysNow
+}
 
 function sortByAuthor() {
     let sortedByAuthor = listOfPosts.sort((a, b) => a.author > b.author ? 1 : -1)
+    document.getElementById("postField").innerHTML = ""
     insertInto(sortedByAuthor)
-
 }
 
 function sortByDate() {
     let sortedByAuthor = listOfPosts.sort((a, b) => a.dateOfPost > b.dateOfPost ? 1 : -1)
+    document.getElementById("postField").innerHTML = ""
     insertInto(sortedByAuthor)
 }
 
 function sortByLikes() {
     let sortedByAuthor = listOfPosts.sort((a, b) => a.likes > b.likes ? -1 : 1)
+    document.getElementById("postField").innerHTML = ""
     insertInto(sortedByAuthor)
+}
+
+function deleteElems(howMuch) {
+    for (let i = 0; i < howMuch; i++) {
+        let fChild = document.querySelector(".mix-blocks")
+        fChild.remove()
+    }
 }
 
 function insertInto(completedArray) {
     const template = document.getElementById("postTemplate")
-    const parentNode = document.getElementById("postField")
-    parentNode.innerHTML = ""
     for (let i = 0; i < completedArray.length; i++) {
         let post = template.content.cloneNode(true)
         post.querySelector("h3").innerText = completedArray[i].author
@@ -42,3 +72,15 @@ function insertInto(completedArray) {
 document.querySelector("#sortAuthor").onclick = sortByAuthor
 document.querySelector("#sortLikes").onclick = sortByLikes
 document.querySelector("#sortDate").onclick = sortByDate
+
+window.onload = requestToServer()
+window.addEventListener("scroll", () => {
+    const docRect = document.documentElement.getBoundingClientRect()
+    if (docRect.bottom < document.documentElement.clientHeight + 400) {
+        requestToServer()
+    }
+    if (loadedAlready > 50) {
+        deleteElems(porcionOfPosts)
+        loadedAlready -= porcionOfPosts
+    }
+})
