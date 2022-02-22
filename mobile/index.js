a60b865d5e81e5e85c13817a8e71b31cc5932531
Eugene <Slickylock@gmail.com>
2022-02-22 16:14:12 +0500

    fix images

--- mobile/index.js
+++ mobile/index.js
@@ -1,6 +1,5 @@
 import { displayPreview } from "./modules/preview.js"
 
 const inputField = document.getElementById("uploadInput")
+
 inputField.addEventListener("change", displayPreview)
-
-
--- mobile/modules/preview.js
+++ mobile/modules/preview.js
@@ -1,13 +1,13 @@
 export function displayPreview() {
     const itsImage = document.getElementById("memePreviewField") || document.getElementById("imagePreviewField")
-    const loader = document.getElementById("uploadMeme") || document.getElementById("uploadInput")
-    const fileImage = loader.files[0]
+    const fileImage = this.files[0]
     if (fileImage) {
         const reader = new FileReader()
         reader.addEventListener("load", () => {
             itsImage.setAttribute("src", reader.result)
             itsImage.setAttribute("width", "100%")
             itsImage.setAttribute("height", "100%")
+            itsImage.style.objectFit = "cover"
         })
         reader.readAsDataURL(fileImage)
         const placeholder = document.querySelector(".placeholderForMeme")
